3828181f89bf9e2d430c33c2cce26af21fc10d7b
atomicchipmonk <spaceterps@gmail.com>
2025-12-29 11:31:37 -0800

    first automation attempt in nix

--- nix-packages/home-assistant.nix
+++ nix-packages/home-assistant.nix
@@ -25,8 +25,30 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
-    };
+      automation = [
+      {
+        alias = "Toggle Announcement";
+        trigger = {
+            platform = "numeric_state";
+            entity_id = "input_boolean.toggle";
+            to = "on";
+        };
+        condition = {};
+        action = {
+            target = {
+                entity_id = "conversation.home_assistant";
+                data = {
+                    message = "Turned Toggle On";
+                    preannounce = true;
+                };
+                
+              };
+            };
+          }
+        ];
+      };
   };
+
 
   services.wyoming.piper.servers.options = {
     enable = true;
