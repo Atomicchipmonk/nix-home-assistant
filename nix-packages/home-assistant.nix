4dc12d63f4aff6af4d0ca526cd6ff31c17ff9965
atomicchipmonk <spaceterps@gmail.com>
2025-12-30 23:56:21 -0800

    models back to nix-ified cache

--- nix-packages/home-assistant.nix
+++ nix-packages/home-assistant.nix
@@ -56,7 +56,7 @@ in
     services.wyoming.openwakeword = {
       enable = true;
       uri = "tcp://0.0.0.0:10400";
-      customModelsDirectories = [ ../models ];
+      customModelsDirectories = [ modelsLocation ];
     };
 
 }
