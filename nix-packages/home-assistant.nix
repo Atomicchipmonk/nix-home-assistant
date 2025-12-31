b705be04d53ab60b0a031a4f327aab27ff2873e0
atomicchipmonk <spaceterps@gmail.com>
2025-12-30 23:35:17 -0800

    add models directory pointer

--- nix-packages/home-assistant.nix
+++ nix-packages/home-assistant.nix
@@ -49,9 +49,7 @@
   services.wyoming.openwakeword = {
     enable = true;
     uri = "tcp://0.0.0.0:10400";
-    customModelsDirectories =
-
-    #vap still no dice.
+    customModelsDirectories = "../models";
   };
 
 }
