eab2e4067e58bc3da8b7cca8454b0ae273491095
atomicchipmonk <spaceterps@gmail.com>
2026-01-01 22:00:52 -0800

    just get vlc happy again

--- aziz-master.nix
+++ aziz-master.nix
@@ -16,7 +16,7 @@
 
     imports = [
         <nixos-hardware/raspberry-pi/4>
-        ./nix-packages/satellite.nix
+        #./nix-packages/satellite.nix
     ];
 
     environment.systemPackages = with pkgs; [
@@ -27,6 +27,9 @@
 
     ###### Hardware Setup - Touch screen Raspi 4 ####### 
 
+    ### GPU ###
+    hardware.raspberry-pi."4".fkms-3d.enable = true;
+
     ### Audio ###
     boot.kernelParams = [ "snd_bcm2835.enable_hdmi=1" "snd_bcm2835.enable_headphones=1" ];
 
@@ -36,19 +39,20 @@
     services.xserver = {
         enable = true;
 
-        # GNOME Desktop Environment
-        displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
-
         # Configure keymap in X11
         xkb.layout = "us";
         xkb.variant = "";
 
-        # Enable touchpad support (enabled default in most desktopManager).
-        libinput.enable = true;
     };
 
+    # Enable touchpad support (enabled default in most desktopManager).
+    services.libinput.enable = true;
+
     services.displayManager = {
+        # GNOME Desktop Environment
+        gdm.enable = true;
+        gnome.enable = true;
+
         autoLogin.enable = true;
         autoLogin.user = "chris";
     };
