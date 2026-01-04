25164af8648da599d446410da56c76372ed0f922
atomicchipmonk <spaceterps@gmail.com>
2026-01-03 17:18:41 -0800

    you can just yeet a sed command into the overlay?!

--- nix-packages/satellite.nix
+++ nix-packages/satellite.nix
@@ -2,6 +2,17 @@
 
 {
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      webrtc-audio-processing_1 = prev.webrtc-audio-processing_1.overrideAttrs (oldAttrs: {
+        postPatch = (oldAttrs.postPatch or "") + ''
+          # Add missing cstdint include
+          sed -i '17a #include <cstdint>' webrtc-audio-processing-1/api/task_queue/task_queue_base.h
+        '';
+      });
+    })
+  ];
+  
   environment.systemPackages  = with pkgs; [
       alsa-utils
     ];
@@ -9,7 +20,7 @@
   services.wyoming.satellite = {
     enable = true;
     uri = "tcp://0.0.0.0:10700";
-    area = "brickhouse";
+    area = "master";
     user = "chris";
     microphone = {
       command = "arecord -r 16000 -c 1 -f S16_LE -t raw";
@@ -18,7 +29,7 @@
       command = "aplay -r 22050 -c 1 -f S16_LE -t raw";
     };
 
-    # still no dice on vap, this just constantly streams mic data
+    # still no dice on vap due to chunk size issue (), this just constantly streams mic data
     vad.enable = false;
 
   };
