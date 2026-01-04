46d0aff89498ce60a89ed7494f0c595419743d88
atomicchipmonk <spaceterps@gmail.com>
2026-01-03 20:42:18 -0800

    lets build an override package

--- nix-packages/satellite.nix
+++ nix-packages/satellite.nix
@@ -1,18 +1,35 @@
 { config, pkgs, libs, utils, ... }:
 
+let
+  fixedWebrtc = pkgs.webrtc-audio-processing_1.overrideAttrs (oldAttrs: {
+    stdenv = pkgs.gcc13Stdenv;
+    # postPatch = (oldAttrs.postPatch or "") + ''
+    #   sed -i '/#include "rtc_base\/thread_annotations.h"/a #include <cstdint>' \
+    #     webrtc-audio-processing-1/api/task_queue/task_queue_base.h
+    # '';
+  });
+in
 {
-
   nixpkgs.overlays = [
     (final: prev: {
-      python313 = prev.python313.override {
-        packageOverrides = pyfinal: pyprev: {
-          webrtc-audio-processing_1 = prev.webrtc-audio-processing_1.override {
-            stdenv = prev.gcc13Stdenv;
-          }; 
-        };
+      wyoming-openwakeword = prev.wyoming-openwakeword.override {
+        webrtc-audio-processing_1 = fixedWebrtc;
       };
     })
   ];
+
+
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     python313 = prev.python313.override {
+  #       packageOverrides = pyfinal: pyprev: {
+  #         webrtc-audio-processing_1 = prev.webrtc-audio-processing_1.override {
+  #           stdenv = prev.gcc13Stdenv;
+  #         }; 
+  #       };
+  #     };
+  #   })
+  # ];
   
   environment.systemPackages  = with pkgs; [
       alsa-utils
