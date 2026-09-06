1b5d33d0aaf324a9127946e5a370568f8cdb111b
atomicchipmonk <spaceterps@gmail.com>
2026-09-06 12:33:14 -0700

    no longer a thing for usb pinning

--- aziz-master.nix
+++ aziz-master.nix
@@ -22,10 +22,6 @@
         ./nix-packages/satellite.nix
     ];
 
-    #Set usb current to max to stop usb mic/hid interference
-    boot.loader.raspberryPi.firmwareConfig = ''
-    usb_max_current_enable=1
-    '';
 
     # satellite area set
     services.wyoming.satellite.area = "master";
